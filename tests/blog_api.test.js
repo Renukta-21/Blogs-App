ebe84b856fec9854d16c7e666832a2c4dcdee970
Daniel Urbina <edu211004@gmail.com>
2024-10-19 15:07:21 -0600

    beforeEach wipe DB

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,25 +1,48 @@
-const {test, describe, after} = require('node:test')
+const { test, describe, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
-const mongoose =  require('mongoose')
+const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../models/blog')
 const app = require('../app')
 
-const api= supertest(app)
+const api = supertest(app)
 
-test('Blogs are returned as json', ()=>{
-    console.log('test entry')
-     return api.get('/api/blogs/')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+const initialBlogs = [
+  {
+    title: 'El hombre que no vive',
+    author: 'Daniel Urbina',
+    url: 'fdfjdfhksdfjfd',
+    likes: 31,
+  },
+  {
+    title: 'El hombre que no vive',
+    author: 'Daniel Urbina',
+    url: 'fdfjdfhksdfjfd',
+    likes: 31,
+  },
+]
 
-test('There are three blogs', ()=>{
-    return api.get('/api/blogs') 
-    .then(response=>{
-        assert.strictEqual(response.body.length, 3)
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogPromises = initialBlogs.map(b => {
+      const newBlog = new Blog(b)
+      return newBlog.save()
     })
+    await Promise.all(blogPromises)
+  })
+  
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
-after(async()=>{
-    await mongoose.connection.close()
+test('First Blog is by Daniel Urbina',async()=>{
+    const response = await api.get('/api/blogs')
+
+    const authors = response.body.map(b=> b.author)
+    assert(authors.includes('Daniel Urbina'))
+})
+after(() => {
+  mongoose.connection.close()
 })
