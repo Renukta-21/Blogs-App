96908a637da579f18803830858f57f722bda03bf
Daniel Urbina <edu211004@gmail.com>
2024-10-21 13:02:28 -0600

    refactoring promises

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,16 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req,res) => {
-    Blog.find({})
-    .then(blogs=> res.send(blogs))
+blogsRouter.get('/', async(req,res) => {
+    const blogs = await Blog.find({})
+    res.send(blogs)
 })
-blogsRouter.post('/', (req,res) => {
+blogsRouter.post('/', async(req,res) => {
     const newBlog = new Blog(req.body)
     console.log(newBlog)
-    newBlog.save()
-    .then(savedBlog=>{
-        res.status(201).send(savedBlog)
-    })
+    const savedBlog = await newBlog.save()
+    res.status(201).send(savedBlog)
 })
 module.exports = blogsRouter
