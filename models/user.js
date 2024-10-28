c72b31a106c313fd843997a5e48384fa7e2e5df8
Daniel Martinez <edu211004@gmail.com>
2024-10-28 16:33:16 -0600

    relational collections

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    username: String,
-    name:String,
-    passwordHash:String
-})
-
-const User = new mongoose.model('user', userSchema)
