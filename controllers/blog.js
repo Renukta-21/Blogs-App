11a22ad388d8d16a7cb69f4cfc9d0bac5ce83263
Daniel Urbina <edu211004@gmail.com>
2024-11-07 02:30:44 -0600

    veryfing user tokens

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,50 +1,64 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-    res.send(blogs)
-})
-blogsRouter.post('/', async (req, res) => {
-    const user = await User.findById(req.body.userId)
+const getToken = (request) => {
+  const authorization = request.get('Authorization')
+  if (authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer', '')
+  }
+  return null
+}
 
-    if (req.body.title && req.body.author && req.body.url) {
-        let newBlog = new Blog({
-            ...req.body,
-            likes: req.body.likes === undefined ? 0 : req.body.likes,
-            user: req.body.userId
-        })
-        const savedBlog = await newBlog.save()
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-        res.status(201).send(savedBlog)
-    } else {
-        res.status(400).send({ error: 'Required fileds not sent' })
-    }
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  res.send(blogs)
+})
+
+blogsRouter.post('/', async (req, res) => {
+  const decodedToken = jwt.verify(getToken(req), process.env.JWT_SECRET_KEY)
+  if(!decodedToken.id){
+    return res.status(401).send({error: 'invalid token'})
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  if (req.body.title && req.body.author && req.body.url) {
+    let newBlog = new Blog({
+      ...req.body,
+      likes: req.body.likes === undefined ? 0 : req.body.likes,
+      user: req.body.userId,
+    })
+    const savedBlog = await newBlog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    res.status(201).send(savedBlog)
+  } else {
+    res.status(400).send({ error: 'Required fileds not sent' })
+  }
 })
 
 blogsRouter.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    const blog = await Blog.findByIdAndDelete(id)
-    if (blog) {
-        res.status(204).end()
-    } else {
-        res.status(404).end()
-    }
-
+  const { id } = req.params
+  const blog = await Blog.findByIdAndDelete(id)
+  if (blog) {
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
 })
 
-blogsRouter.put('/:id',async (req,res)=>{
-    console.log(req.body)
-    const {id} = req.params
-    const updatedBlog = req.body
-    const blog = await Blog.findByIdAndUpdate(id, updatedBlog, {new:true})
-    if(blog){
-        res.status(200).send(blog)
-    }else{
-        res.status(404).send({error:'Blog not found'})
-    }
+blogsRouter.put('/:id', async (req, res) => {
+  console.log(req.body)
+  const { id } = req.params
+  const updatedBlog = req.body
+  const blog = await Blog.findByIdAndUpdate(id, updatedBlog, { new: true })
+  if (blog) {
+    res.status(200).send(blog)
+  } else {
+    res.status(404).send({ error: 'Blog not found' })
+  }
 })
 
 module.exports = blogsRouter
